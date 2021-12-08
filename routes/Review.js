bbd5f507854b332deaee43c7173dca949d08c178
Thomas Ismaïl Murzyn <thomasismailmurzyn@MacBook-Pro-de-Thomas.local>
2021-12-08 14:37:45 +0100

    added route get review add and less review

--- routes/Review.js
+++ routes/Review.js
@@ -33,11 +33,63 @@ router.post("/review/:id", isAuthenticated, async (req, res) => {
     } else {
       res
         .status(400)
-        .json({ error: "You have already posted a review ont this game" });
+        .json({ error: "You have already posted a review on this game" });
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
     console.log(error.message);
+  }
+});
+
+router.get("/get_review/:id", async (req, res) => {
+  try {
+    console.log("route /get_review/:id");
+
+    const reviews = await Review.find({ gameId: req.params.id }).populate(
+      "user"
+    );
+
+    if (reviews) {
+      res.status(200).json(reviews);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post("/add_score_review/:id", async (req, res) => {
+  try {
+    console.log("route /add_score_review/:id");
+
+    const review = await Review.findOne({
+      gameId: req.params.id,
+      user: req.fields.user,
+    });
+
+    if (review) {
+      review.score = review.score + req.fields.score;
+      review.save();
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post("/less_score_review/:id", async (req, res) => {
+  try {
+    console.log("route /less_score_review/:id");
+
+    const review = await Review.findOne({
+      gameId: req.params.id,
+      user: req.fields.user,
+    });
+
+    if (review) {
+      review.score = review.score - req.fields.score;
+      review.save();
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 });
 
